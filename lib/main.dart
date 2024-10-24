6ee56d50d56941c7d0661f7bbb20cb37c1ec7974
Marco Bavagnoli <marco.bavagnoli@gmail.com>
2024-10-24 22:17:47 +0200

    process command changed

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,46 @@ void main() {
   runApp(const MyApp());
 }
 
+/// The channels to be used while initializing the player.
+enum Channels {
+  /// One channel.
+  mono(1),
+
+  /// Two channels.
+  stereo(2),
+
+  /// Four channels.
+  quad(4),
+
+  /// Six channels.
+  surround51(6),
+
+  /// Eight channels.
+  dolby71(8);
+
+  const Channels(this.count);
+
+  /// The channels count.
+  final int count;
+
+  /// Returns a human-friendly channel name.
+  @override
+  String toString() {
+    switch (this) {
+      case Channels.mono:
+        return 'Mono';
+      case Channels.stereo:
+        return 'Stereo';
+      case Channels.quad:
+        return 'Quad';
+      case Channels.surround51:
+        return 'Surround 5.1';
+      case Channels.dolby71:
+        return 'Dolby 7.1';
+    }
+  }
+}
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -37,12 +77,9 @@ class _MyHomePageState extends State<MyHomePage> {
   late LocalProcessManager localProcess;
   Process? process;
   List<String> command = [];
-  bool nativeFrameRate = true;
-  final codec = ['mp3', 'flac', 'ogg', 'wav', 'pcm'];
+  double nativeFrameRate = 0;
   final sampleRate = [11025, 22050, 44100, 48000];
-  final channels = [1, 2];
   final format = ['f32le', 's8', 's16le', 's32le'];
-  int codecId = 4;
   int srId = 2;
   int chId = 0;
   int fmtId = 0;
@@ -60,6 +97,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   void dispose() {
+    outputController.dispose();
     if (process != null) {
       localProcess.killPid(process!.pid);
     }
@@ -86,72 +124,66 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   List<String> composeCommand() {
-    // The FFmpeg's "-re" flag means to "Read input at native frame rate"
+    // -readrate speed (input)
+    // Limit input read speed.
+    // Its value is a floating-point positive number which represents the
+    // maximum duration of media, in seconds, that should be ingested in
+    // one second of wallclock time. Default value is zero and represents
+    // no imposed limitation on speed of ingestion. Value 1 represents real-time
+    // speed and is equivalent to -re.
+    // Mainly used to simulate a capture device or live input stream
+    // (e.g. when reading from a file). Should not be used with a low value
+    // when input is an actual capture device or live stream as it may cause
+    // packet loss.
+    // It is useful for when flow speed of output packets is important,
+    // such as live streaming.
+    //
+    // 5 will ingest 5 seconds of media data in one second
+
     command.clear();
-    if (codec[codecId] == 'pcm') {
-      final acodec = switch (fmtId) {
-        0 => '-acodec pcm_f32le',
-        1 => '',
-        2 => '-acodec pcm_s16le',
-        3 => '-acodec pcm_s32le',
-        _ => '',
-      };
-      command.add('/bin/bash');
-      command.add('-c');
-      command.add('websocketd --port=8080 --binary=true '
-          'ffmpeg '
-          '${nativeFrameRate ? '-re ' : ''} '
-          '-i "$audioPath" '
-          '-f ${format[fmtId]} $acodec -ac ${channels[chId]} '
-          '-ar ${sampleRate[srId]} -');
-    } else {
-      final f = switch (codecId) {
-        0 => '-f mp3',
-        1 => '-f flac',
-        2 => '-f ogg',
-        3 => '-f wav',
-        _ => '',
-      };
-      command.add('/bin/bash');
-      command.add('-c');
-      command.add('websocketd --port=8080 --binary=true '
-          'ffmpeg '
-          '${nativeFrameRate ? '-re ' : ''} '
-          '-i "$audioPath" '
-          '$f -');
-    }
+    final acodec = switch (fmtId) {
+      0 => '-acodec pcm_f32le',
+      1 => '',
+      2 => '-acodec pcm_s16le',
+      3 => '-acodec pcm_s32le',
+      _ => '',
+    };
+    command.add('/bin/bash');
+    command.add('-c');
+    command.add('websocketd --port=8080 --binary=true '
+        'ffmpeg '
+        '-readrate ${(nativeFrameRate * 10).floorToDouble() / 10} '
+        '-i "$audioPath" '
+        '-f ${format[fmtId]} $acodec -ac ${Channels.values[chId].count} '
+        '-ar ${sampleRate[srId]} -');
     return command;
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: const Text('websocketd'),
-      ),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
+              
               children: [
-                /// CODEC
+                /// SAMPLERATE
                 Column(
                   mainAxisSize: MainAxisSize.min,
                   children: [
-                    for (var i = 0; i < codec.length; i++)
+                    for (var i = 0; i < sampleRate.length; i++)
                       SizedBox(
                         width: 160,
                         child: RadioListTile<int>(
-                          title: Text(codec[i]),
+                          title: Text(sampleRate[i].toString()),
                           value: i,
-                          groupValue: codecId,
-                          onChanged: (value) {
+                          groupValue: srId,
+                          onChanged: (int? value) {
                             setState(() {
-                              codecId = value!;
+                              srId = value!;
                               composeCommand();
                               startProcess();
                             });
@@ -161,165 +193,86 @@ class _MyHomePageState extends State<MyHomePage> {
                   ],
                 ),
 
-                /// SAMPLERATE
-                if (codec[codecId] == 'pcm')
-                  Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      for (var i = 0; i < sampleRate.length; i++)
-                        SizedBox(
-                          width: 160,
-                          child: RadioListTile<int>(
-                            title: Text(sampleRate[i].toString()),
-                            value: i,
-                            groupValue: srId,
-                            onChanged: (int? value) {
-                              setState(() {
-                                srId = value!;
-                                composeCommand();
-                                startProcess();
-                              });
-                            },
-                          ),
-                        ),
-                    ],
-                  ),
-
                 /// CHANNELS
-                if (codec[codecId] == 'pcm')
-                  Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      for (var i = 0; i < channels.length; i++)
-                        SizedBox(
-                          width: 160,
-                          child: RadioListTile<int>(
-                            title: Text(channels[i].toString()),
-                            value: i,
-                            groupValue: chId,
-                            onChanged: (int? value) {
-                              setState(() {
-                                chId = value!;
-                                composeCommand();
-                                startProcess();
-                              });
-                            },
-                          ),
+                Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    for (var i = 0; i < Channels.values.length; i++)
+                      SizedBox(
+                        width: 210,
+                        child: RadioListTile<int>(
+                          title: Text(Channels.values[i].toString()),
+                          value: i,
+                          groupValue: chId,
+                          onChanged: (int? value) {
+                            setState(() {
+                              chId = value!;
+                              composeCommand();
+                              startProcess();
+                            });
+                          },
                         ),
-                    ],
-                  ),
+                      ),
+                  ],
+                ),
 
                 /// FORMAT
-                if (codec[codecId] == 'pcm')
-                  Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      for (var i = 0; i < format.length; i++)
-                        SizedBox(
-                          width: 160,
-                          child: RadioListTile<int>(
-                            title: Text(format[i]),
-                            value: i,
-                            groupValue: fmtId,
-                            onChanged: (int? value) {
-                              setState(() {
-                                fmtId = value!;
-                                composeCommand();
-                                startProcess();
-                              });
-                            },
-                          ),
+                Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    for (var i = 0; i < format.length; i++)
+                      SizedBox(
+                        width: 160,
+                        child: RadioListTile<int>(
+                          title: Text(format[i]),
+                          value: i,
+                          groupValue: fmtId,
+                          onChanged: (int? value) {
+                            setState(() {
+                              fmtId = value!;
+                              composeCommand();
+                              startProcess();
+                            });
+                          },
                         ),
-                    ],
-                  ),
-              ],
-            ),
-
-            Wrap(
-              runSpacing: 8,
-              spacing: 8,
-              children: [
-                OutlinedButton(
-                  onPressed: () {
-                    outputController.text = '';
-                    Process.start('/bin/bash', ['-c', 'killall -9 websocketd']);
-                  },
-                  child: const Text('kill ALL websocketd'),
-                ),
-                OutlinedButton(
-                  onPressed: () {
-                    setState(() {
-                      command.clear();
-                      command.add('/bin/bash');
-                      command.add('-c');
-                      command.add('websocketd --port=8080 --binary=true '
-                        'cat /home/deimos/pppppp_44100_1_s8.pcm');
-                      startProcess();
-                    });
-                  },
-                  child: const Text('cat pppppp_44100_1_s8.pcm'),
-                ),
-                OutlinedButton(
-                  onPressed: () {
-                    setState(() {
-                      command.clear();
-                      command.add('/bin/bash');
-                      command.add('-c');
-                      command.add('websocketd --port=8080 --binary=true '
-                        'cat /home/deimos/ppppp_44100_s16_1.pcm');
-                      startProcess();
-                    });
-                  },
-                  child: const Text('cat ppppp_44100_s16_1.pcm'),
-                ),
-                OutlinedButton(
-                  onPressed: () {
-                    setState(() {
-                      command.clear();
-                      command.add('/bin/bash');
-                      command.add('-c');
-                      command.add('websocketd --port=8080 --binary=true '
-                        'cat /home/deimos/pppppp_f32_1_44100.pcm');
-                      startProcess();
-                    });
-                  },
-                  child: const Text('cat pppppp_f32_1_44100.pcm'),
-                ),
-                OutlinedButton(
-                  onPressed: () {
-                    setState(() {
-                      command.clear();
-                      command.add('/bin/bash');
-                      command.add('-c');
-                      command.add('websocketd --port=8080 --binary=true '
-                        'cat /home/deimos/ppppppp.flac');
-                      startProcess();
-                    });
-                  },
-                  child: const Text('cat ppppppp.flac'),
+                      ),
+                  ],
                 ),
               ],
             ),
 
+            OutlinedButton(
+              onPressed: () {
+                outputController.text = '';
+                Process.start('/bin/bash', ['-c', 'killall -9 websocketd']);
+              },
+              child: const Text('kill ALL websocketd'),
+            ),
             const SizedBox(height: 8),
+
             Row(
               children: [
-                Switch(
+                Slider(
                   value: nativeFrameRate,
+                  min: 0,
+                  max: 4,
                   onChanged: (value) {
                     setState(() {
                       nativeFrameRate = value;
+                      composeCommand();
                       startProcess();
                     });
                   },
                 ),
-                const Text('Read input at native frame rate'),
+                Text('${nativeFrameRate.toStringAsFixed(1)}X    '
+                    '1 = real-time speed '
+                    'at native frame rate. 0 no limitation on speed'),
               ],
             ),
             const SizedBox(height: 8),
 
             TextField(
-              controller: TextEditingController(text: command.join(' ') ?? ''),
+              controller: TextEditingController(text: command.join(' ')),
               style: const TextStyle(fontSize: 12),
             ),
             const SizedBox(height: 8),
@@ -327,7 +280,7 @@ class _MyHomePageState extends State<MyHomePage> {
             /// OUTPUT
             TextField(
               controller: outputController,
-              maxLines: 10,
+              maxLines: 12,
               style: const TextStyle(fontSize: 12),
               decoration: InputDecoration(
                 fillColor: Colors.grey[100],
